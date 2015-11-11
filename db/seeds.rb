e5f7014bb6a04ee8d6751e8b8bc6eb32564f1d55
tunganh123 <louistunganh@gmail.com>
2015-11-11 11:36:23 +0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create name:  "A",
+            email: "a@gmail.com",
+            password:  "123456",
+            password_confirmation: "123456"
+      
